7a34cf8396c134049d179dac429003efeb13eeca
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-11-13 07:56:53 +0900

    ci: test using old tflint

---
+++ tests/old/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "foo" {}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.0"
+    }
+  }
+}
